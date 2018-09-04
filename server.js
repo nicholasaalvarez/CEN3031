ed434b2b6e95626ef275f81ebc07196535e2088f
Nicholas Alvarez <nicholasalvarez@Nicholass-MacBook-Pro.local>
2018-09-04 11:27:58 -0400

    Update Files

--- server.js
+++ server.js
@@ -16,11 +16,31 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+    
+    
+    if(parsedUrl.path == '/listings'){
+        response.writeHead(200);
+        response.end(listingData);
+    }
+    else
+    {
+        response.writeHead(404);
+        response.end("Bad gateway error")
+    }
 };
 
+server = http.createServer(requestHandler);
+
+
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
-});
+            /*
+             This callback function should save the data in the listingData variable,
+             then start the server.
+             */
+            if(err){
+            throw err;
+            }
+            
+            listingData = data;
+            server.listen(8080);
+            });
